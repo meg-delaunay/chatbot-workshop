9b335b86879c11e4512fcd710ed6867197247ca7
Megan DeLaunay <mld719@Megans-MacBook-Pro.local>
2017-09-21 13:09:53 -0400

    empty out all the good stuff

--- express-server.js
+++ express-server.js
@@ -1,43 +1,33 @@
 var express = require('express')
 var apiai = require("apiai");
-
 var responses = require("./generateResponse.js")
 
-
 var app = express()
-var agent = apiai("6f3707fd712e443eb20c8eb33a798e0a");
+var agent = apiai("7366b0843c4549bab1c1279ebaef473e"); // <-- YOUR CLIENT ID HERE
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
 });
 
 app.use(express.static(__dirname + '/static'));
 
 
-let necessaryFields = {
-  location: '',
-  foodType: '',
-  mealTime: ''
+//dumby caching system
+let slots = {
+
 }
 
 app.get('/response', function (req, res) {
     var text = req.query.utterance
 
-    console.log(JSON.stringify(text));
-
-    var request = agent.textRequest(text, {sessionId: 'convo1'});
+    var request = agent.textRequest(text, {sessionId: 'convo1'}); // --> GENERATE DYNAMICALLY
 
     request.on('response', function(response) {
-        console.log('Intent: ' + response.result.metadata.intentName);
-        console.log(response.result.fulfillment.speech);
+        var messageBack = responses.generateResponse(response.result, slots)
 
-        var messageBack = responses.generateResponse(response.result, necessaryFields)
-
-        console.log(messageBack.response);
-
-        necessaryFields = messageBack.fields
+        slots = messageBack.fields
         res.send(messageBack.response)
     });
 
@@ -49,5 +39,5 @@ app.get('/response', function (req, res) {
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!')
 })
--- generateResponse.js
+++ generateResponse.js
@@ -1,66 +1,20 @@
 
-var generateResponse = function(data, fields) {
+var generateResponse = function(data, slots) {
     console.log(data);
     var response = ''
 
-    fields = fillSlots(data, fields)
+    slots = fillSlots(data, slots)
 
     // build out the business logic your own way.
     // any combination of slots. be as specific as you want.
 
-    console.log(fields);
-
-    if(fields.location && fields.mealTime && fields.foodType) {
-        console.log("in the if statement");
-        response = "Okay. " + fields.foodType + ' for ' + fields.mealTime + " in " + fields.location + '? ' +
-                    "I can do that! Have you ever tried... ______"
-        console.log(response);
-    } else if (fields.location && fields.mealTime) {
-        response = "What type of food do you want?"
-    } else if (fields.location && fields.foodType) {
-        response = "Breakfast, Lunch, or Dinner?"
-    } else if (fields.foodType && fields.mealTime) {
-        response = "And where in Richmond are you looking to eat?"
-    } else if (fields.location) {
-        response = "What type of food are you looking for?"
-    } else if (fields.mealTime) {
-        response = "What type of food are you looking for?"
-    } else if (fields.foodType) {
-        response = "Breakfast, lunch, or dinner?"
-    } else {
-        response = 'Okay. To start, is this breakfast lunch or dinner?'
-    }
-
-
-    return {response: response, fields:fields}
+    return {response: 'your chatbot is working!', fields:slots}
 }
 
 fillSlots = function(data, slots) {
 
-    //if the user starts over, clear the slots.
-    if(data.action == 'FindRestaurant') {
-        slots.location = ''
-        slots.mealTime = ''
-        slots.foodType = ''
-    }
-
-    if(data.parameters.mealTime) {
-        slots.mealTime = data.parameters.mealTime
-    }
-
-    if(data.parameters.neighborhoods) {
-        slots.location = data.parameters.neighborhoods
-    }
-
-    if(data.parameters.foodType) {
-        slots.foodType = data.parameters.foodType
-    }
-
-
-    console.log(slots);
     return slots
 
 }
-
 
 module.exports.generateResponse = generateResponse
