cdcd15d6a49db46a1acbcb376cd453f80775e3c2
Megan DeLaunay <mld719@Megans-MacBook-Pro.local>
2017-09-21 13:17:19 -0400

    more shell

--- generateResponse.js
+++ generateResponse.js
@@ -1,14 +1,24 @@
 
 var generateResponse = function(data, slots) {
     console.log(data);
-    var response = ''
+    let response = 'Your chatbot is working, with no intent found.'
 
     slots = fillSlots(data, slots)
 
     // build out the business logic your own way.
     // any combination of slots. be as specific as you want.
 
-    return {response: 'your chatbot is working!', fields:slots}
+    // usually some combination of:
+    //      checking intent/action
+    //      checking which slots are already filled
+    //      checking which slots need to be added
+    //      emptying or resetting slots
+
+    if(data.action == '<Intent_Name>') {
+        response = 'Your chatbot found an intent.'
+    }
+
+    return {response: response, fields:slots}
 }
 
 fillSlots = function(data, slots) {
